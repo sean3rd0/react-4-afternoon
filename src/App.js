527eeeb9cff61385b60052694ffac1fae16ee6a1
Shannon Scott <sean3rd0@icloud.com>
2019-10-23 09:35:17 -0600

    finished React-4

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
+import {HashRouter as RS} from "react-router-dom";
+import routes from "./routes";
+import {Link} from "react-router-dom";
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <nav className='nav'>
-          <div>WestSide University</div> 
-          <div className='link-wrap'>
-              <div className='links'>Home</div>
-              <div className='links'>About</div> 
-          </div>
-        </nav>
-      </div>
+      <RS>
+        <div>
+          <nav className='nav'>
+            <div>WestSide University</div> 
+            <div className='link-wrap'>
+                <Link to="/" className='links'>Home</Link>
+                <Link to="/about" className='links'>About</Link> 
+            </div>
+          </nav>
+          {routes}
+        </div>
+      </RS>
     )
   }
 }
--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react';
+import axios from "axios"
 
 export default class Student extends Component {
   constructor() {
     super()
 
+    this.state = {
+      studentInfo: {}
+    }
+  }
+
+  componentDidMount(){
+    axios.get(`http://localhost:3005/students/${this.props.match.params.id}`)
+    .then(response => {
+      this.setState({
+        studentInfo: response.data
+      })
+    })
   }
 
   render() {
+    const {studentInfo} = this.state
     return (
       <div className="box">
         <h1>Student</h1>
+        <h1>{studentInfo.first_name} {studentInfo.last_name}</h1>
+        <h3>Grade: {studentInfo.grade}</h3>
+        <h3>Email: {studentInfo.email}</h3>
       </div>
     )
   }
